4ae57502c72afd41bdab2e2d4def92f02c121ebf
lobthere <lobthere@protonmail.com>
2025-05-04 14:34:45 +0200

    up

--- Chimie/Capacite_numerique_1.py
+++ Chimie/Capacite_numerique_1.py
@@ -1,23 +1,41 @@
-import matplotlib
 import numpy as np
+import matplotlib.pyplot as plt
 
 pK_a1 = 1.25
-pK_a2 = 4.3
+Ka1 = 10**-1.25
+Ka2 = 10**-4.3
 T = 298
 
-pH = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-x, y, z = [], [], []
+"""--- 1 ---"""
+pH = []
+pas = 10000
+for m in range(0, pas, 1):
+    pH.append(m*14/pas)
+x, y, z, w = [], [], [], []
+
+"""--- 2 ---"""
+
 for i in range(0, len(pH)):
     to_solve = np.array([
         [1, 1, 1],
-        [-1 * pK_a1, 10**(-1 * pH[i]), 0],
-        [-1 * pK_a2, 0, 10 ** (-1 * pH[i])]
+        [-1 * Ka1, 10**(-1 * pH[i]), 0],
+        [0, -1 * Ka2, 10 ** (-1 * pH[i])]
     ])
 
     the_values = np.array([100, 0, 0])
 
     the_answer = np.linalg.solve(to_solve, the_values)
-    x.append(the_answer[0])
-    y.append(the_answer[1])
-    z.append(the_answer[2])
-print(x)
+
+    x.append(the_answer[0]/(the_answer[0] + the_answer[1] + the_answer[2])*100)
+    y.append(the_answer[1]/(the_answer[0] + the_answer[1] + the_answer[2])*100)
+    z.append(the_answer[2]/(the_answer[0] + the_answer[1] + the_answer[2])*100)
+    w.append([the_answer[0] + the_answer[1] + the_answer[2], the_answer[0], the_answer[1], the_answer[2]])
+
+print(w)
+
+"""--- 3 ---"""
+
+plt.plot(pH, x)
+plt.plot(pH, y)
+plt.plot(pH, z)
+plt.show()
