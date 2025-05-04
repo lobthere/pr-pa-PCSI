27a73d42f717be4555dbe584ab93c96feb2523a7
lobthere <lobthere@protonmail.com>
2025-05-04 15:14:57 +0200

    fin

--- Chimie/Capacite_numerique_1.py
+++ Chimie/Capacite_numerique_1.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+"""___Exercice 1___"""
+
 pK_a1 = 1.25
 Ka1 = 10**-1.25
 Ka2 = 10**-4.3
@@ -9,7 +11,7 @@ T = 298
 """--- 1 ---"""
 pH = []
 pas = 10000
-for m in range(0, pas, 1):
+for m in range(0, pas + 1, 1):
     pH.append(m*14/pas)
 x, y, z, w = [], [], [], []
 
@@ -31,11 +33,56 @@ for i in range(0, len(pH)):
     z.append(the_answer[2]/(the_answer[0] + the_answer[1] + the_answer[2])*100)
     w.append([the_answer[0] + the_answer[1] + the_answer[2], the_answer[0], the_answer[1], the_answer[2]])
 
-print(w)
 
 """--- 3 ---"""
 
-plt.plot(pH, x)
-plt.plot(pH, y)
-plt.plot(pH, z)
+plt.plot(pH, x, label='H2A')
+plt.plot(pH, y, label='HA-')
+plt.plot(pH, z, label='A2-')
+plt.xlabel('pH')
+plt.ylabel('%')
+plt.title("diagramme de distribution de l'acide oxalique H2A")
+plt.legend()
 plt.show()
+
+
+
+
+"""___Exercice 2___"""
+
+"""--- 1 ---"""
+
+T = 298
+pKe = 14
+Ks = 10**(-18.5)
+C = 10**(-2)
+pas = 1000
+
+pH = []
+for i in range(0, pas + 1, 1):
+    pH.append(i*14/pas)
+
+Cu , Cu2 = [], []
+
+"""--- 2 ---"""
+
+for i in range(0, len(pH), 1):
+    HO = 10 ** (pH[i] - pKe)
+    if C*(HO**2) > Ks:
+        Cu2_pourcent = ((Ks/(HO**2))/C)*100
+        Cu2.append(Cu2_pourcent)
+        Cu.append(100-Cu2_pourcent)
+    else:
+        Cu2.append(100)
+        Cu.append(0)
+
+"""--- 3 ---"""
+
+plt.plot(pH, Cu2, label='Cu2+')
+plt.plot(pH, Cu, label='Cu(OH)2(s)')
+plt.xlabel('pH')
+plt.ylabel('%')
+plt.title("diagramme de distribution de Cu2+/Cu(OH)2(s)")
+plt.legend()
+plt.show()
+
